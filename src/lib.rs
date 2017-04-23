1d9fc255f515f803b362469bf3d0183e34359ef2
Fan Jiang <i@fanjiang.me>
2017-04-23 19:12:05 +0800

    Working Xargo-based setup for rust

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+#![feature(lang_items, core_intrinsics)]
+#![feature(start)]
+#![no_std]
+
+#![crate_type="staticlib"]
+
+use core::intrinsics;
+
+// Entry point for this program.
+#[no_mangle] // ensure that this symbol is called `main` in the output
+pub extern fn rust_main(_argc: i32, _argv: *const *const u8) -> i32 {
+    0
+}
+
+// These functions are used by the compiler, but not
+// for a bare-bones hello world. These are normally
+// provided by libstd.
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern fn rust_eh_personality() {
+}
+
+// This function may be needed based on the compilation target.
+#[lang = "eh_unwind_resume"]
+#[no_mangle]
+pub extern fn rust_eh_unwind_resume() {
+}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn rust_begin_panic(_msg: core::fmt::Arguments,
+                               _file: &'static str,
+                               _line: u32) -> ! {
+    unsafe { intrinsics::abort() }
+}
